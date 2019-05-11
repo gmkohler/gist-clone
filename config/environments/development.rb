05a193b8216b6f7a8f26e7f6d8c49afe44376dd6
Gregory Kohler <kohler@squareup.com>
2019-05-11 16:03:46 -0700

    configure database/ORM

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.sequel.schema_format = :sql
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
